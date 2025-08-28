a0ec1b356cc63d945892a5f40394d2d06c9dba84
Paulo-gitdev <coutinhop916@gmail.com>
2025-08-28 16:01:33 -0300

    Delete site_V5 directory

--- site_V5/arquivos_php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Inicia a sessão
-session_unset(); // Remove todas as variáveis da sessão
-session_destroy(); // Destroi a sessão
-header('Location: ../index.html'); // Redireciona para a página inicial
-exit();
-?>
